43a3e60abf16e32ee3b5c3ae3e21696992a60643
stasSajin <sajin.stanislav@gmail.com>
2016-05-26 16:25:53 -0700

    test post

--- _scripts/knitpages.R
+++ _scripts/knitpages.R
@@ -6,23 +6,20 @@
 
 # run ./knitpages.R to update all knitr files that need to be updated.
 
-KnitPost <- function(input, outfile, figsfolder, cachefolder, base.url="/") {
+KnitPost <- function(input, outfile, figsfolder, base.url="/") {
         # this function is a modified version of an example here:
         # http://jfisher-usgs.github.com/r/2012/07/03/knitr-jekyll/
         require(knitr);
         opts_knit$set(base.url = base.url)
         fig.path <- paste0(figsfolder, sub(".Rmd$", "", basename(input)), "/")
-        cache.path <- file.path(cachefolder, sub(".Rmd$", "", basename(input)), "/")
-        
         opts_chunk$set(fig.path = fig.path)
-        opts_chunk$set(cache.path = cache.path)
         opts_chunk$set(fig.cap = "center")
         render_jekyll()
         knit(input, outfile, envir = parent.frame())
         # brocks::htmlwidgets_deps(input, always = TRUE)
 }
 
-knit_folder <- function(infolder, outfolder, figsfolder, cachefolder) {
+knit_folder <- function(infolder, outfolder, figsfolder) {
         for (infile in list.files(infolder, pattern = "*.Rmd", full.names = TRUE)) {
                 pattern = "\\d\\d\\d\\d\\-\\d\\d\\-\\d\\d\\-"
                 print(infile)
@@ -33,10 +30,10 @@ knit_folder <- function(infolder, outfolder, figsfolder, cachefolder) {
                 # knit only if the input file is the last one modified
                 if (!file.exists(outfile) |
                     file.info(infile)$mtime > file.info(outfile)$mtime) {
-                        KnitPost(infile, outfile, figsfolder, cachefolder)
+                        KnitPost(infile, outfile, figsfolder)
                 }
         }
 }
 
-knit_folder("_R", "_posts", "figs/", "_caches/")
+knit_folder("_R", "_posts", "figs/")
 #knit_folder("_R/drafts", "_drafts", "figs/drafts/")
